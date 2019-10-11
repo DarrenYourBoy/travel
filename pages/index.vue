50af407dcdaa3cf14b7d5a2b4ec35836d03248fb
DarrenYourBoy <53564212+DarrenYourBoy@users.noreply.github.com>
2019-10-11 23:13:45 +0800

    初始化

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <span>攻略</span>
+        <span>酒店</span>
+        <span>机票</span>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
